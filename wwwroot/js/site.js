611b84ee34b62727fdf3f89d765525c08059c3e2
Bruno <brunoco56@hotmail.com>
2021-10-02 18:58:19 -0300

    adicionados vários eventos e chamadas Javascript com jqueryt

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,77 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+// pegando dados da tela usando JAVASCRIP PURO
+var teste = document.getElementById("botao");
 
-// Write your JavaScript code.
+teste.onclick = function () {
+    alert("botao pressionado -->   " + $('#botao').html());
+};
+
+$("#botao").click(function () {
+    $("#texto").hide();
+});
+
+
+
+// pegando dados da tela usando JQUERY
+var texto = $('#texto').html();
+console.log(texto);
+
+
+//Inserindo mais dados no DOM com Jquery
+$('#texto').append(' <p id="testenovo">TExto adicionado através do append</p>').html();
+console.log($('#testenovo').html());
+
+
+//Pegando dados da tela com .CLASS
+var pegarTexto = $('.display-4').html();
+console.log(pegarTexto);
+
+
+//Adicionando após
+$('.display-4').after('<p> Texto adicionado após o título através de JQUERY == after</p>');
+
+//Função de clique no Título da Pagina
+var contador = 0;
+$('.display-4').click(
+    function () {        
+        contador = contador + 1;
+      
+        alert(pegarTexto + " foi clicado " + contador + " vezes");
+
+
+        //  Este comando abaixo remove
+        //$("#segundaLinha").remove();
+
+        let input = window.prompt("Um simples Input para adicionar dado na segunda DIV");
+        console.log(input);
+        $("#segundaLinha").first().html(input);
+    }
+);
+
+
+///   Before == antes     e After == Depois
+
+$("#segundaLinha").before('<h5> texto da segunda DIV adicionado antes </h5> ');
+
+// Aqui abaixo acionamos quando o conteúdo é totlamente carregado
+ window.onload = function () { alert("Alerta do window.load, dizendo que a estará a ser carregada.") }
+
+
+// para acionarmos o evento ates da página ser totalmente carregada usaríamos:
+// $(document).read(alert("Antes do documento ser carregado"));
+// foi abreviado por:
+$(function () {
+    alert(" abreviado ");
+});
+
+
+
+//Evento do CLick
+
+$('ul').on('click', 'li',
+    function ()
+    {
+        console.log("li clicado");
+
+    });
+$('li').append('<li>Adicionao depois de clicar<li>');
